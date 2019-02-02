c1eeb29e364aff59191829f91a904a79102f6d02
Max Ivanov <maxlvan@yandex.ru>
2019-02-02 22:15:31 +0300

    Lesson: @HostListener and host Object

--- src/app/modules/directives/directives/credit-card.directive.ts
+++ src/app/modules/directives/directives/credit-card.directive.ts
@@ -1,9 +1,28 @@
-import {Directive, ElementRef} from '@angular/core';
+import {Directive, ElementRef, HostListener} from '@angular/core';
 
 @Directive({
   selector: '[credit-card]'
 })
 export class CreditCardDirective {
+
+  @HostListener('input', ['$event']) onInput(event) {
+    const input = <HTMLInputElement>event.target;
+
+    const value = input.value;
+
+    let trimmed = value.replace(/\s/g, '');
+
+    if (trimmed.length > 16) {
+      trimmed = trimmed.substr(0, 16);
+    }
+
+    const numbers = [];
+    for (let i = 0; i < trimmed.length; i += 4) {
+      numbers.push(trimmed.substr(i, 4));
+    }
+
+    input.value = numbers.join(' ');
+  }
 
   constructor(private el: ElementRef) {
     console.log(el);
