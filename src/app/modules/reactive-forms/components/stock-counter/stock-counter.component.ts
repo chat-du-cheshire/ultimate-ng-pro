82a0681261c17d3fcb285a2aebb512a131d47a61
Max Ivanov <maxlvan@yandex.ru>
2019-04-17 22:58:58 +0300

    Lesson: Implementing a ControlValueAccessor

--- src/app/modules/reactive-forms/components/stock-counter/stock-counter.component.ts
+++ src/app/modules/reactive-forms/components/stock-counter/stock-counter.component.ts
@@ -1,11 +1,22 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, forwardRef, Input, OnInit} from '@angular/core';
+import {ControlValueAccessor, NG_VALUE_ACCESSOR} from '@angular/forms';
+
+const STOCK_COUNTER_ACCESSOR = {
+  provide: NG_VALUE_ACCESSOR,
+  useExisting: forwardRef(() => StockCounterComponent),
+  multi: true
+};
 
 @Component({
   selector: 'stock-counter',
   templateUrl: './stock-counter.component.html',
-  styleUrls: ['./stock-counter.component.scss']
+  styleUrls: ['./stock-counter.component.scss'],
+  providers: [STOCK_COUNTER_ACCESSOR]
 })
-export class StockCounterComponent implements OnInit {
+export class StockCounterComponent implements ControlValueAccessor {
+
+  private onTouched: Function;
+  private onValueChanged: Function;
 
   @Input() step = 10;
   @Input() min = 10;
@@ -13,22 +24,34 @@ export class StockCounterComponent implements OnInit {
 
   value = 10;
 
-  constructor() {
+  writeValue(value: number): void {
+    this.value = value;
   }
 
-  ngOnInit() {
+  registerOnChange(fn: any): void {
+    this.onValueChanged = fn;
+  }
+
+  registerOnTouched(fn: any): void {
+    this.onTouched = fn;
   }
 
   onIncrement() {
     if (this.value < this.max) {
       this.value += this.step;
+      this.onValueChanged(this.value);
     }
+
+    this.onTouched();
   }
 
   onDecrement() {
     if (this.value > this.min) {
       this.value -= this.step;
+      this.onValueChanged(this.value);
     }
+
+    this.onTouched();
   }
 
 }
