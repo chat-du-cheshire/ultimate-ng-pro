d761c7b77e0b47bd575070d09973f590cf1b7e58
Ivanov-mo <Ivanov-MO@gaz-is.ru>
2019-06-05 18:08:05 +0300

    Testing Component methods

--- src/app/modules/reactive-forms/components/stock-counter/stock-counter.component.spec.ts
+++ src/app/modules/reactive-forms/components/stock-counter/stock-counter.component.spec.ts
@@ -16,10 +16,49 @@ describe('StockCounterComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(StockCounterComponent);
     component = fixture.componentInstance;
+    component.registerOnChange(() => {});
+    component.registerOnTouched(() => {});
+    component.min = 0;
+    component.max = 2;
+    component.step = 1;
+    component.value = 0;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should increment correctly', () => {
+    component.onIncrement();
+    expect(component.value).toEqual(1);
+  });
+
+  it('should decrement correctly', () => {
+    component.onIncrement();
+    expect(component.value).toEqual(1);
+
+    component.onDecrement();
+    expect(component.value).toEqual(0);
+  });
+
+  it('should decrement correctly', () => {
+    component.onIncrement();
+    expect(component.value).toEqual(1);
+
+    component.onDecrement();
+    expect(component.value).toEqual(0);
+  });
+
+  it('should not decrement below the minimum value', () => {
+    component.onIncrement();
+    expect(component.value).toBe(1);
+    component.onDecrement();
+    expect(component.value).toBe(0);
+    component.onDecrement();
+    expect(component.value).toBe(0);
+  });
+
+  it('should not increment below the maximum value', () => {
+    component.onIncrement();
+    component.onIncrement();
+    component.onIncrement();
+    expect(component.value).toBe(2);
   });
 });
