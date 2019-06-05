721ee8e0d7ebb7f49ffbbceb7a13f7dc8e386d02
Ivanov-mo <Ivanov-MO@gaz-is.ru>
2019-06-05 18:48:45 +0300

    Testing Component templates

--- src/app/modules/reactive-forms/components/stock-counter/stock-counter.component.spec.ts
+++ src/app/modules/reactive-forms/components/stock-counter/stock-counter.component.spec.ts
@@ -1,10 +1,13 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { StockCounterComponent } from './stock-counter.component';
+import {DebugElement} from '@angular/core';
+import {By} from '@angular/platform-browser';
 
 describe('StockCounterComponent', () => {
   let component: StockCounterComponent;
   let fixture: ComponentFixture<StockCounterComponent>;
+  let debug: DebugElement;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -15,6 +18,7 @@ describe('StockCounterComponent', () => {
 
   beforeEach(() => {
     fixture = TestBed.createComponent(StockCounterComponent);
+
     component = fixture.componentInstance;
     component.registerOnChange(() => {});
     component.registerOnTouched(() => {});
@@ -22,6 +26,9 @@ describe('StockCounterComponent', () => {
     component.max = 2;
     component.step = 1;
     component.value = 0;
+
+    debug = fixture.debugElement;
+
     fixture.detectChanges();
   });
 
@@ -60,5 +67,20 @@ describe('StockCounterComponent', () => {
     component.onIncrement();
     component.onIncrement();
     expect(component.value).toBe(2);
+  });
+
+  it('should increment when the + button is clicked', () => {
+    debug.query(By.css('button:first-child')).triggerEventHandler('click', null);
+    fixture.detectChanges();
+    expect(component.value).toBe(1);
+    expect(debug.query(By.css('p')).nativeElement.textContent).toBe('1');
+  });
+
+  it('should increment the value when the up arrow is pressed', () => {
+    const event = new Event('KeyboardEvent') as any;
+    event.code = 'ArrowUp';
+    debug.query(By.css('.stock-counter > div > div')).triggerEventHandler('keydown', event);
+    fixture.detectChanges();
+    expect(component.value).toBe(1);
   });
 });
