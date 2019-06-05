f4696d378a5263b54a2f7288144113f024485c1f
Ivanov-mo <Ivanov-MO@gaz-is.ru>
2019-06-05 16:32:04 +0300

    Testing shallow Pipes

--- src/app/modules/pipes/pipes/to-mb.pipe.spec.ts
+++ src/app/modules/pipes/pipes/to-mb.pipe.spec.ts
@@ -1,4 +1,6 @@
 import { ToMbPipe } from './to-mb.pipe';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
+import {Component} from '@angular/core';
 
 describe('ToMbPipe', () => {
   describe('ToMbPipe isolated test', () => {
@@ -9,6 +11,50 @@ describe('ToMbPipe', () => {
 
       expect(pipe.transform(123456789, 'Megabytes')).toEqual('117.74Megabytes');
       expect(pipe.transform(987654321, 'Mbytes')).toEqual('941.90Mbytes');
+    });
+  });
+
+  describe('ToMbPipe shallow test', () => {
+
+    @Component({
+      template: `Size: {{size | toMb : suffix}}`
+    })
+    class TestComponent {
+      size = 123456789;
+      suffix: string;
+    }
+
+    let component: TestComponent;
+    let fixture: ComponentFixture<TestComponent>;
+    let el: HTMLElement;
+
+    beforeEach(() => {
+      TestBed.configureTestingModule({
+        declarations: [TestComponent, ToMbPipe]
+      });
+
+      fixture = TestBed.createComponent(TestComponent);
+      component = fixture.componentInstance;
+      el = fixture.nativeElement;
+    });
+
+    it('should convert bytes to megabytes', () => {
+      fixture.detectChanges();
+      expect(el.textContent).toContain('Size: 117.74MB');
+      component.size = 1029281;
+      fixture.detectChanges();
+      expect(el.textContent).toContain('Size: 0.98MB');
+    });
+
+    it('should use the default extension when not supplied', () => {
+      fixture.detectChanges();
+      expect(el.textContent).toContain('Size: 117.74MB');
+    });
+
+    it('should override the extension when supplied', () => {
+      component.suffix = 'myExt';
+      fixture.detectChanges();
+      expect(el.textContent).toContain('Size: 117.74myExt');
     });
   });
 });
